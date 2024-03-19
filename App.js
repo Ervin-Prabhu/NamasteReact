7022d2d8e2327be9a1ae4a08ae136592c56a45ae
Ervin Prabhu <ervinprabhu5@gmail.com>
2024-03-20 04:10:32 +0530

    episode 2

--- App.js
+++ App.js
@@ -1,18 +1,5 @@
-// const heading = React.createElement("h1",{id:"heading"},"Hello World from React!");
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// console.log(root);
-// root.render(heading);
-
-{/* <div id="parent">
-    <div id="child1">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div> */}
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 const parent = React.createElement("div",{id:"parent"},
                 [React.createElement("div",{id:"child1"},
@@ -23,9 +10,3 @@ const parent = React.createElement("div",{id:"parent"},
                     React.createElement("h2",{},"I'm h2 tag")])])
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
-
-// const header = React.createElement("h1",{},"I'm ");
-// const child = React.createElement("div",{id:"child"}, header);
-// const parent = React.createElement("div",{id:"parent"},child);
-// const root = ReactDOM.createRoot(document.getElementById("root"));
-// root.render(parent);
